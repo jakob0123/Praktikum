7dcd39d1abff304ed5c388a0b447b628739ad0a5
jakob.prem <jakob.prem@josephinum.at>
2022-06-20 23:30:14 +0200

    Schulungstool Backup

---
+++ DAMONE.asdf/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using DAMONE.asdf.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DAMONE.asdf.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddSchulungen()
+        {
+            return View();
+        }
+        public IActionResult SeeSchulungen()
+        {
+            return View();
+        }
+        public IActionResult AddSignature()
+        {
+            return View();
+        }
+        public IActionResult AddPowerPoint()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
